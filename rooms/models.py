5292742ec28a853d1456a4a1e9491b10755887b8
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-05-23 00:03:19 +0200

    Add topics and user relations to room model

--- rooms/models.py
+++ rooms/models.py
@@ -1,8 +1,15 @@
 from unicodedata import name
 from django.db import models
+from django.conf import settings
+
+from topics.models import Topic
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
