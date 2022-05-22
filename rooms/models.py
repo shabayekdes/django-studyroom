21d36b458343f6b2cc10581bdefcd1b84ca897f4
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-05-22 23:18:42 +0200

    Add room model and migrations

---
+++ rooms/models.py
@@ -0,0 +1,12 @@
+from unicodedata import name
+from django.db import models
+
+# Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- studyroom/settings.py
+++ studyroom/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rooms.apps.RoomsConfig',
 ]
 
 MIDDLEWARE = [
