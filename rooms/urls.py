8aaaf665ab4979e52b970b845b0fe6f75ea589fa
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-05-23 22:54:18 +0200

    Add update room page

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import (
     list_rooms,
-    create_room
+    create_room,
+    update_room
 )
 
 urlpatterns = [
     path('', list_rooms, name='list-rooms'),
     path('create/', create_room, name='create-room'),
+    path('<int:id>/edit/', update_room, name='update-room'),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import RoomForm
 from .models import Room
@@ -20,3 +20,15 @@ def create_room(request):
         'form': form,
     }
     return render(request, 'rooms/create.html', context=context)
+
+def update_room(request, id=None):
+    obj = get_object_or_404(Room, pk=id)
+    form = RoomForm(instance=obj, data=request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('list-rooms')
+    context = {
+        'form': form,
+        'object': obj,
+    }
+    return render(request, 'rooms/update.html', context=context)
