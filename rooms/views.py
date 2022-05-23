107e94015d34bcc17f8559419e623248b28925f5
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-05-23 22:16:39 +0200

    Add create room page

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import RoomForm
 
 # Create your views here.
 def create_room(request):
-    return render(request, 'rooms/create.html')
+    form = RoomForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect(request, 'rooms/list.html', {'form': form})
+    context = {
+        'form': form,
+    }
+    return render(request, 'rooms/create.html', context=context)
