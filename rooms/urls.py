b8ea763c512c2500d3aaf3d406fbf6a17d7595d0
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-05-23 22:39:18 +0200

    Add list rooms page

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from .views import (
+    list_rooms,
     create_room
 )
 
 urlpatterns = [
+    path('', list_rooms, name='list-rooms'),
     path('create/', create_room, name='create-room'),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, redirect
 
 from .forms import RoomForm
+from .models import Room
 
 # Create your views here.
+def list_rooms(request):
+    obj = Room.objects.all()
+    context = {
+        'objects': obj
+    }
+    return render(request, 'rooms/list.html', context)
+
 def create_room(request):
     form = RoomForm(request.POST or None)
     if form.is_valid():
         form.save()
-        return redirect(request, 'rooms/list.html', {'form': form})
+        return redirect('list-rooms')
     context = {
         'form': form,
     }
