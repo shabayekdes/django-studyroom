4d0a7f3cf6cfde92784af282797c33f5023161a4
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-05-28 15:12:35 +0200

    Change view room page and sperated list room to anthor components

--- chat_messages/forms.py
+++ chat_messages/forms.py
@@ -6,3 +6,16 @@ class MessageForm(ModelForm):
     class Meta:
         model = Message
         fields = ['body']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # django-crispy-forms
+        for field in self.fields:
+            new_data = {
+                "placeholder": 'Write your message here...',
+                # "class": 'form-control',
+            }
+            self.fields[str(field)].widget.attrs.update(
+                new_data
+            )
+        self.fields['body'].widget.attrs.update({'rows': '2'})
